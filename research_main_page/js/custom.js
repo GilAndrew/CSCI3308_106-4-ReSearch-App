ddbd988b58885c8962cfd40ac6bb1f132a195974
Doug311 <doth4793@colorado.edu>
2019-03-14 16:33:15 -0600

    Cleaning up

--- research_main_page/js/custom.js
+++
@@ -1,7 +0,0 @@
-// HOME ARROW //
-
-// arrow fades away as you scroll
-$(window).scroll(function() {
-    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
-        // 250: pixel fade
-    });
