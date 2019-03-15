1e501dfd327f6c33d68cf5c6d0803c01253c7151
Doug311 <doth4793@colorado.edu>
2019-03-14 18:16:48 -0600

    Javascript/alert popup with info from form

--- ReSearch/resources/js/register_scripts.js
+++ ReSearch/resources/js/register_scripts.js
@@ -20,8 +20,23 @@ function student_form_called()
 	/*
 	going to have to manually insert student value
 	*/
-	console.log("student called");
-	alert("The student form was submitted");
+	Name = document.getElementById("Student-js").elements["Name"];
+	User_name = document.getElementById("Student-js").elements["User-name"];
+	Email = document.getElementById("Student-js").elements["Email"];
+	Year = document.getElementById("Student-js").elements["Year"];
+	Birthday = document.getElementById("Student-js").elements["Birthday"];
+	Password = document.getElementById("Student-js").elements["Password"];
+	Confirm_Password = document.getElementById("Student-js").elements["Confirm-Password"];
+
+	console.log("Student Called: \n Name: " + Name.value + "\n User-name: " + User_name.value +
+	"\n Email: " + Email.value + "\n Year: "+ Year.value + "\n Birthday: "
+	+ Birthday.value + "\n Password: "+ Password.value + "\n Confirm_Password: "
+	+ Confirm_Password.value);
+
+	alert("Student Name: " + Name.value + "\n User-name: " + User_name.value +
+	"\n Email: " + Email.value + "\n Year: "+ Year.value + "\n Birthday: "
+	+ Birthday.value + "\n Password: "+ Password.value + "\n Confirm_Password: "
+	+ Confirm_Password.value);
 }
 
 function researcher_form_called()
@@ -29,6 +44,14 @@ function researcher_form_called()
 	/*
 	going to have to manually insert the researcher value
 	*/
-	console.log("researcher called");
-	alert("The  researcher form was submitted");
+	Email = document.getElementById("Researcher-js").elements["Email"];
+	Name = document.getElementById("Researcher-js").elements["Name"];
+	Password = document.getElementById("Researcher-js").elements["Password"];
+	Confirm_Password = document.getElementById("Researcher-js").elements["Confirm-Password"];
+
+	console.log("Researcher called: \n Email: " + Email.value + "\n Name: " + Name.value +
+	"\n Password: " + Password.value + "\n Confirm_Password: " + Confirm_Password.value);
+
+	alert("Researcher Email: "+ Email.value + "\n Name: " + Name.value +
+	"\n Password: " + Password.value + "\n Confirm_Password: " + Confirm_Password.value);
 }
