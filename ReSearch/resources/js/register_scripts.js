286315319a50f024bfc0fef48be7c8a7a4244f9a
lukesoguero <lukesoguero@gmail.com>
2019-03-18 15:00:41 -0600

    Added ability to register as a user

--- ReSearch/resources/js/register_scripts.js
+++ ReSearch/resources/js/register_scripts.js
@@ -20,23 +20,25 @@ function student_form_called()
 	/*
 	going to have to manually insert student value
 	*/
-	Name = document.getElementById("Student-js").elements["Name"];
-	User_name = document.getElementById("Student-js").elements["User-name"];
-	Email = document.getElementById("Student-js").elements["Email"];
-	Year = document.getElementById("Student-js").elements["Year"];
-	Birthday = document.getElementById("Student-js").elements["Birthday"];
-	Password = document.getElementById("Student-js").elements["Password"];
-	Confirm_Password = document.getElementById("Student-js").elements["Confirm-Password"];
+	var Name = document.getElementById("Student-js").elements["Name"];
+	var User_name = document.getElementById("Student-js").elements["User-name"];
+	var Email = document.getElementById("Student-js").elements["Email"];
+	var Year = document.getElementById("Student-js").elements["Year"];
+	var Birthday = document.getElementById("Student-js").elements["Birthday"];
+	var Password = document.getElementById("Student-js").elements["Password"];
+	var Confirm_Password = document.getElementById("Student-js").elements["Confirm-Password"];
 
-	console.log("Student Called: \n Name: " + Name.value + "\n User-name: " + User_name.value +
-	"\n Email: " + Email.value + "\n Year: "+ Year.value + "\n Birthday: "
-	+ Birthday.value + "\n Password: "+ Password.value + "\n Confirm_Password: "
-	+ Confirm_Password.value);
-
-	alert("Student Name: " + Name.value + "\n User-name: " + User_name.value +
-	"\n Email: " + Email.value + "\n Year: "+ Year.value + "\n Birthday: "
-	+ Birthday.value + "\n Password: "+ Password.value + "\n Confirm_Password: "
-	+ Confirm_Password.value);
+	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+	xmlhttp.open("POST", "http://localhost:3000/user_registration/send_form", true);
+	xmlhttp.setRequestHeader("Content-Type", "application/json");
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			console.log(this.response)
+			
+		}
+	}
+	xmlhttp.send(JSON.stringify({name:Name.value, username:User_name.value, email:Email.value, year:Year.value, birthday:Birthday.value, password:Password.value, confirm_password:Confirm_Password.value}));
+	alert("Submitted! Profile won't be added to database without including this alert. Working on fixing that.");
 }
 
 function researcher_form_called()
