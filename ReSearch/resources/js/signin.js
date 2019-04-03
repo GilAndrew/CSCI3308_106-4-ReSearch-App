bc876e0c416ba8d231690f02faf68f216fd8f3d8
miro6181 <mike.rogers1272@gmail.com>
2019-04-02 20:32:41 -0600

    Login button fix

--- ReSearch/resources/js/signin.js
+++ ReSearch/resources/js/signin.js
@@ -2,7 +2,7 @@ function login(callback) {
 	var Email = document.getElementById("sign_in_form").elements["inputEmail"];
 	var Password = document.getElementById("sign_in_form").elements["inputPassword"];
 
-	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance
 	xmlhttp.open("POST", "http://localhost:3000/student_login", true);
 	xmlhttp.setRequestHeader("Content-Type", "application/json");
 	xmlhttp.onreadystatechange = function() {
@@ -24,4 +24,12 @@ function login(callback) {
 function toHomepage() {
 	// redirects to homepage
 	location.href = "file:///home/luke/Documents/CSCI3308_106-4-ReSearch-App/ReSearch/views/index.html";
+}
+
+function switchToStudent() {
+	document.getElementById("email-field").placeholder = "Student Email Address";
+}
+
+function switchToResearcher() {
+	document.getElementById("email-field").placeholder = "Researcher Email Address";
 }
