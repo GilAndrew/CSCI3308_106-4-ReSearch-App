1bde45a2c12bbd401937dc3b2ecbad0f5ed6ca5c
Al <afsm1995@live.com>
2019-04-06 13:49:08 -0600

    Small changes

--- ReSearch/resources/js/custom.js
+++ ReSearch/resources/js/custom.js
@@ -2,6 +2,6 @@
 
 // arrow fades away as you scroll
 $(window).scroll(function() {
-    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 450); 
         // 250: pixel fade
     });
