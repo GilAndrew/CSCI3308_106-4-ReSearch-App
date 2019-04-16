91e7a72f152030b199eda2e9f11557380aad003f
Doug311 <doth4793@colorado.edu>
2019-04-16 09:56:24 -0600

    Able to Reteive and set Variable Sucessfully in JS (Edit Capabilities)

--- ReSearch/resources/js/student_profile.js
+++ ReSearch/resources/js/student_profile.js
@@ -1,4 +1,4 @@
-/* For Profile Picture */
+/* Handles Profile Picture Upload */
 $(document).ready(function() {
 
     var readURL = function(input) {
@@ -22,27 +22,40 @@ $(document).ready(function() {
     });
 });
 
-function Sample_Populate()
+/*The Following Variables Should be initialised when the webpage loads by the database
+values. They will be changed by the heading-to-profile function, which is called after edits are submitted*/
+var Image = "https://pbs.twimg.com/media/DTJLmSAW4AIMBrg.jpg"; /* Probably would be best to set a default pic in DB when they register */
+var Email = "idk@idk.com";
+var Birthday = "10";
+var Year = "Sophmore";
+var Username = "Username";
+var Full_Name = "Douglas Thomas";
+
+
+/* Not sure how post operation work in JS, but this would be a good place to put
+the function which Grabs the information from database on Load Up*/
+function load_from_DB()
 {
-    console.log("In Sample_Populate");
-    
 
-    var Email = "sample@sample.com";
-    var Username = "Sample_Username";
-    var Birthdate = "01/02/2003";
-    var Year = "Junior";
-    var Name = "Douglas Thomas";
-
-    document.getElementById("Email").innerHTML = Email;
-    document.getElementById("Birthdate").innerHTML = Birthdate;
-    document.getElementById("Year").innerHTML = Year;
-    document.getElementById("Username").innerHTML = Username;
-    document.getElementById("Name").innerHTML = Name;
 }
 
+/* Function is called when webpage load to set element to correct values,
+and is called again one data is changed in edit function*/
+function get_correct_data()
+{
+    document.getElementById("Profile-Image").src = Image;
+    document.getElementById("Email").innerHTML = Email;
+    document.getElementById("Birthday").innerHTML = Birthday;
+    document.getElementById("Year").innerHTML = Year;
+    document.getElementById("Username").innerHTML = Username;
+    document.getElementById("Name").innerHTML = Full_Name;
+    console.log("Image: " + Image);
+}
+
+/* Deals with the visibility of each form */
 function edit_profile(id, toggle)
 {
-    console.log("In edit_profile");
+    console.log("Toggling Visibility of Fourms");
     if(toggle == 0)
 	{
 		document.getElementById(id).style.visibility = "hidden";
@@ -53,15 +66,27 @@ function edit_profile(id, toggle)
 	{
 		document.getElementById(id).style.visibility = "visible";
 		document.getElementById(id).style.height = "auto";
-	}	
+    }	
 }
 
 function headingToEdit()
 {
-    //populate place Holders with individuals information
+    /* populate place Holders with individuals information */
+    console.log("Heading To Edit Fourm");
 }
 
 function headingToProfile()
 {
-    //going to be similar to submit form for registration + update the data on profile page
+    console.log("Heading To Profile Fourm");
+    Image = document.getElementById("Edit-Image").src;
+    Email = document.getElementById("edit-profile-js").elements["Edit-Email"].value;
+    Birthday = document.getElementById("edit-profile-js").elements["Edit-Birthday"].value;
+    Year = document.getElementById("edit-profile-js").elements["Edit-Year"].value;
+    Username = document.getElementById("edit-profile-js").elements["Edit-Username"].value;
+    get_correct_data(); /* Will Update what the user see's imediately */
+
+    /* These Updated Values should be sent and Stored In database Here...
+     Note I will be adding JS similar to that of the register page, such that we 
+     submit the correct data each time*/
 }
+
