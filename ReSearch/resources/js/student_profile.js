6dc5f627024edd9aa87f362b59399aa815b9511b
Doug311 <doth4793@colorado.edu>
2019-04-17 20:34:59 -0600

    All javascript I Plan to have is in here now, just minor css changes needed, and probably something to prevent an sql attack from inputs as well

--- ReSearch/resources/js/student_profile.js
+++ ReSearch/resources/js/student_profile.js
@@ -26,17 +26,18 @@ $(document).ready(function() {
 values. They will be changed by the heading-to-profile function, which is called after edits are submitted*/
 var Image = "https://pbs.twimg.com/media/DTJLmSAW4AIMBrg.jpg"; /* Probably would be best to set a default pic in DB when they register */
 var Email = "idk@idk.com";
-var Birthday = "10";
-var Year = "Sophmore";
+var Birthday = "01-01-2000";
+var Year = "Sophmore"; //need to add conditionals so that FSH isnt displayed, but freshman
 var Username = "Username";
-var Full_Name = "Douglas Thomas";
+var Full_Name = "Individuals Name";
 
 
 /* Not sure how post operation work in JS, but this would be a good place to put
-the function which Grabs the information from database on Load Up*/
+the function which Grabs the information from database on Load Up and
+places data from DB into global variables above*/
 function load_from_DB()
-{
-
+{ 
+    console.log("this");
 }
 
 /* Function is called when webpage load to set element to correct values,
@@ -49,7 +50,8 @@ function get_correct_data()
     document.getElementById("Year").innerHTML = Year;
     document.getElementById("Username").innerHTML = Username;
     document.getElementById("Name").innerHTML = Full_Name;
-    console.log("Image: " + Image);
+    //console.log("Image: " + Image);
+    console.log("then this");
 }
 
 /* Deals with the visibility of each form */
@@ -71,22 +73,54 @@ function edit_profile(id, toggle)
 
 function headingToEdit()
 {
-    /* populate place Holders with individuals information */
+    /* populate place Holders with individuals information If time permits
+    Ill add the functionality*/
     console.log("Heading To Edit Fourm");
 }
 
 function headingToProfile()
 {
-    console.log("Heading To Profile Fourm");
+    console.log("Save Edits  fucntion Called");
+
+    /* Set DOM Elements to temp variables before assigned to actual variables (excluding Image thats fine) */
     Image = document.getElementById("Edit-Image").src;
-    Email = document.getElementById("edit-profile-js").elements["Edit-Email"].value;
-    Birthday = document.getElementById("edit-profile-js").elements["Edit-Birthday"].value;
-    Year = document.getElementById("edit-profile-js").elements["Edit-Year"].value;
-    Username = document.getElementById("edit-profile-js").elements["Edit-Username"].value;
+
+    Temp_Email = document.getElementById("edit-profile-js").elements["Edit-Email"].value;
+    Temp_Birthday = document.getElementById("edit-profile-js").elements["Edit-Birthday"].value;
+    Temp_Year = document.getElementById("edit-profile-js").elements["Edit-Year"].value;
+    Temp_Username = document.getElementById("edit-profile-js").elements["Edit-Username"].value;
+    
+    /* Making sure data is clean (Only setting if real Input) */
+    if (Temp_Email != ''){
+        Email = Temp_Email;
+    } 
+    if (Temp_Year != 'Select Year of Education'){
+        Year = Temp_Year;
+    }
+    if (Temp_Birthday != '2000-01-01'){
+        Birthday = Temp_Birthday;
+    }
+    if (Temp_Username != ''){
+        Username = Temp_Username;
+    }
+
+    console.log("Data Entry Succefull, will be entered to DB")
     get_correct_data(); /* Will Update what the user see's imediately */
 
-    /* These Updated Values should be sent and Stored In database Here...
-     Note I will be adding JS similar to that of the register page, such that we 
-     submit the correct data each time*/
+    /* Visibility Calls if data is good to sumbit */
+    edit_profile('edit-profile',0); 
+    edit_profile('profile',1);
+
+    /* Clear input fields here since no page reload */
+    document.getElementById("edit-profile-js").elements["Edit-Email"].value = '';
+    document.getElementById("edit-profile-js").elements["Edit-Birthday"].value = '2000-01-01';
+    document.getElementById("edit-profile-js").elements["Edit-Username"].value = '';
+    $('#DropDown option').prop('selected', function() {
+        return this.defaultSelected;
+    });
+
+    /* These Updated Values should be sent and Stored In database Here...*/
 }
+
+
 
