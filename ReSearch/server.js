2b64944b6417f3342c171aac2ab206e67f429857
GilAndrew <gilfandrew@gmail.com>
2019-04-18 21:13:31 -0600

    clarified view engine

--- ReSearch/server.js
+++ ReSearch/server.js
@@ -32,8 +32,8 @@ const dbConfig = process.env.DATABASE_URL; //test this
 
 var db = pgp(dbConfig);
 
-app.set('view engine', 'html'); //test this
-app.use(express.static(__dirname + '/'))
+app.set('view engine', 'ejs'); //test this
+app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res) {
     res.render('/views/index.html',{
