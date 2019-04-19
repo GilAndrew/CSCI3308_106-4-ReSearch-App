0abcbe155aa8a21198934a4f0ee354e546abc77b
GilAndrew <gilfandrew@gmail.com>
2019-04-19 16:09:27 -0600

    more file path changes

--- ReSearch/server.js
+++ ReSearch/server.js
@@ -40,7 +40,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.get('/index.html', function(req, res) {
